5c04a7b1e5a0829bfacdeba33522d0dfd255d0e3
ndewon <mohammad_baragbah@telkomsel.co.id>
2024-04-13 20:31:32 +0700

    build: setup swager and global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('My Task Boards')
+    .setDescription('MyTaskBoard API Description')
+    .setVersion('1.0')
+    .addTag('taskboard')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
